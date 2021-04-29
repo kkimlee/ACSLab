0a080896f33bcef9a3f2cf35064d6377392d3a26
kkimlee <40078267+kkimlee@users.noreply.github.com>
2021-04-29 13:44:38 +0900

    Update eda.py

--- 의료데이터/eda.py
+++ 의료데이터/eda.py
@@ -16,7 +16,8 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 
 from sklearn.metrics import confusion_matrix, recall_score, accuracy_score, precision_score, fbeta_score
-
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
 
 def search(dirname, extension):
     file_list = []
@@ -31,6 +32,16 @@ def search(dirname, extension):
 
 raw_data = pd.read_excel('의료 데이터.xlsx')
 data = raw_data.fillna(0)
+
+AGE = data['age 60']
+for i in range(len(AGE)):
+    age = AGE.iloc[i]
+    if age == 0:
+        AGE.iloc[i] = 1
+    elif age == 1:
+        AGE.iloc[i] = -1
+    else:
+        AGE.iloc[i] = 0
 
 MLH1 = data['MLH1']
 for i in range(len(MLH1)):
@@ -97,7 +108,7 @@ for i in range(len(_3M)):
     elif _3m[-2:] == 'PR':
         _3M.iloc[i] = -1
     elif _3m[-2:] == 'PD':
-        _3M.iloc[i] = --3
+        _3M.iloc[i] = -3
     elif _3m[-2:] == 'CR':
         _3M.iloc[i] = 1
     else:
@@ -108,7 +119,7 @@ _6M = data['6M']
 for i in range(len(_6M)):
     _6m = str(_6M.iloc[i])
     if _6m[-2:] == 'SD':
-        _6M.iloc[i] = -2
+        _6M.iloc[i] = _3M.iloc[i]
     elif _6m[-2:] == 'PR':
         _6M.iloc[i] = -1
     elif _6m[-2:] == 'PD':
@@ -123,7 +134,7 @@ _9M = data['9M']
 for i in range(len(_9M)):
     _9m = str(_9M.iloc[i])
     if _9m[-2:] == 'SD':
-        _9M.iloc[i] = -2
+        _9M.iloc[i] = _6M.iloc[i]
     elif _9m[-2:] == 'PR':
         _9M.iloc[i] = -1
     elif _9m[-2:] == 'PD':
@@ -138,7 +149,7 @@ Result = data['result']
 for i in range(len(_9M)):
     result = str(Result.iloc[i])
     if result == 'SD':
-        Result.iloc[i] = -2
+        Result.iloc[i] = _9M.iloc[i]
     elif result == 'PR':
         Result.iloc[i] = -1
     elif result == 'PD':
@@ -149,7 +160,7 @@ for i in range(len(_9M)):
         Result.iloc[i] = 0
 data['result'] = pd.to_numeric(data['result'])
 
-Relapse = data['재발여부']
+Relapse = data['Relapse']
 for i in range(len(Relapse)):
     relapse = Relapse.iloc[i]
     if relapse == 1:
@@ -158,7 +169,7 @@ for i in range(len(Relapse)):
         Relapse[i] = 1
     else:
         Relapse[i] = 0
-data['재발여부'] = pd.to_numeric(data['재발여부'])
+data['Relapse'] = pd.to_numeric(data['Relapse'])
 
 data = data.drop(columns='재발시점')
 data = data.drop(columns='사망여부')
@@ -170,20 +181,8 @@ fig.set_size_inches(8,6)
 plt.title('Heat map')
 plt.show()
 
-train_data = data.drop(columns=['3M', '6M', '9M', 'result', '재발여부'])
-train_data_label = data['9M']
-
-GradientBoosting_model = GradientBoostingClassifier()
-GradientBoosting_model.fit(train_data, train_data_label)
-result_GradientBoosting_model = GradientBoosting_model.predict(train_data)
-GradientBoosting_model_cm = confusion_matrix(train_data_label, result_GradientBoosting_model)
-print(GradientBoosting_model_cm)
-
-LinearSVC_model = LinearSVC()
-LinearSVC_model.fit(train_data, train_data_label)
-result_LinearSVC_model = LinearSVC_model.predict(train_data)
-LinearSVC_model_cm = confusion_matrix(train_data_label, result_LinearSVC_model)
-print(LinearSVC_model_cm)
+train_data = data.drop(columns=['3M', '6M', '9M', 'result', 'Relapse'])
+train_data_label = data['Relapse']
 
 LR_model = LogisticRegression()
 LR_model.fit(train_data, train_data_label)
@@ -191,8 +190,20 @@ result_LR_model = LR_model.predict(train_data)
 LR_model_cm = confusion_matrix(train_data_label, result_LR_model)
 print(LR_model_cm)
 
+LinearSVC_model = LinearSVC()
+LinearSVC_model.fit(train_data, train_data_label)
+result_LinearSVC_model = LinearSVC_model.predict(train_data)
+LinearSVC_model_cm = confusion_matrix(train_data_label, result_LinearSVC_model)
+print(LinearSVC_model_cm)
+
 RF_model = RandomForestClassifier()
 RF_model.fit(train_data, train_data_label)
 result_RF_model = RF_model.predict(train_data)
 RF_model_cm = confusion_matrix(train_data_label, result_RF_model)
 print(RF_model_cm)
+
+GradientBoosting_model = GradientBoostingClassifier()
+GradientBoosting_model.fit(train_data, train_data_label)
+result_GradientBoosting_model = GradientBoosting_model.predict(train_data)
+GradientBoosting_model_cm = confusion_matrix(train_data_label, result_GradientBoosting_model)
+print(GradientBoosting_model_cm)
